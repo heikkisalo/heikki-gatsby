a147a038866e0970939d50c41b47239195c31cee
Heikki Salo <heikki.salo@superhelio.com>
2017-12-03 21:41:25 +0200

    Google analytics installed, maybe?

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,20 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-  plugins: [`gatsby-plugin-stylus`],
+  plugins: [
+  	{
+  		resolve: `gatsby-plugin-react-helmet`,
+  	},
+  	{
+  		resolve: `gatsby-plugin-stylus`,
+  	},
+  	{
+	    resolve: `gatsby-plugin-google-analytics`,
+	    options: {
+	      trackingId: "UA-82164985-1",
+	      // Setting this parameter is optional
+	      anonymize: true,
+	    },
+	  },
+  ],
 }
